7779d637f0af51ec4f6d98e86a957dca3ff2be3a
Yoshua-G12 <juareyoshua95@gmail.com>
2025-05-19 02:51:35 -0600

    Agregar archivos de proyecto.

---
+++ Models/Alimento.cs
@@ -0,0 +1,20 @@
+﻿using Lab12YG.Models;
+using Lab12YG.Validations;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab12YG.Models
+{
+    public class Alimento : Producto
+    {
+        [Required]
+        [Display(Name = "Fecha de Vencimiento")]
+        [FechaMayorAHoy] // Validación personalizada
+        public DateTime FechaVencimiento { get; set; }
+
+        [Required]
+        [Display(Name = "Unidad de Medida")]
+        public string UnidadMedida { get; set; }
+
+    }
+}
---
+++ Models/Libro.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab12YG.Models
+{
+    public class Libro : Producto
+    {
+        public string Autor { get; set; }
+        public string Editorial { get; set; }
+        public string Categoria { get; set; }
+
+    }
+}
